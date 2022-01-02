3f005dc19ffb057dc938e44ec8d4ee3131d14af7
Achintya Gaumat <30311965+vibrantachintya@users.noreply.github.com>
2022-01-02 17:47:41 +0530

    Create 4_RotateMatrix90Deg.py

---
+++ Arrays/4_RotateMatrix90Deg.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+        
